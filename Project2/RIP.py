4c66bfd642da0c4de405bf34eeaf5283e588b4da
WarGravy <zackcarlson13@gmail.com>
2014-12-03 21:22:41 -0800

    Fixed documentation and structure

--- Project2/RIP.py
+++ Project2/RIP.py
@@ -14,23 +14,8 @@ class NetworkSimulation(object):
 		self.netMap={"132.192.192.68": rip.Router("132.192.192.68"), "132.192.192.67": rip.Router("132.192.192.67")}
 		#build a network map
 
-	def advertise(self, router):
-		"""Will send out advertisements to each of the nodes connected to this."""
-		for neighbor in self.netMap[router][1]: #foreach router that this router is connected to
-			neighborupdate=False #new bool
-			for subnet in self.netMap[router][0]: #foreach destination in the dictionary of destinations of the router
-				if subnet not in self.netMap[neighbor][0]: #if this subnet is not in their particular table, it looks into their table and adds a hop
-					self.netMap[neighbor][0][subnet] = (router,self.netMap[router][0][subnet][1] + 1) 
-					neighborupdate=True
-				elif self.netMap[neighbor][0][subnet][1] > self.netMap[router][0][subnet][1] + 1: #if the router you are connected to doesn't have a better router, fix it  
-					self.netMap[neighbor][0][subnet] = (router,self.netMap[router][0][subnet][1] + 1)
-					neighborupdate=True
-			if neighborupdate:
-				self.netMap[neighbor][3]=True #mark it as updated
-		self.netMap[router][2]=False #marks current router as no longer advertising
-
 	def iterate(self):
-		"""Iterate through all of the routers on the network and send one RIP advertisement each."""
+		"""Iterate through all of the routers on the network that are advertising and has each router advertise to each of it's neighbors."""
 		# Initialize change in router table for this iteration.
 		for router in self.netMap:
 			self.netMap[router].bUpdated=False
